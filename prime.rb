4c500eae9dca9483b4eef87a36cc3d0cc870c46b
Lydia <lreitzel@gmail.com>
2021-01-19 20:21:46 -0800

    commit tree 68b43f99489ef4c10fbb3cf0399c637ecb9123c6

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+    if number < 2
+        return false
+    else
+        factors = (2..number-1).to_a.select do |n|
+            number % n == 0
+        end
+        if factors.length == 0
+            return true
+        else return false
+        end
+    end
+end
